302789303cfd35accc5cd9683b218b086482fad4
Alice M <alice@alicem.net>
2019-05-13 21:27:28 +0200

    feat(gps): use exiv2 to dump metadata, then add location info in HTML

--- generate.sh
+++ generate.sh
@@ -7,11 +7,17 @@
 
 set -e
 
+# Set to an empty string to avoid displaying location information.
+declare -r SHOULD_USE_GPS_INFO=1
+#declare -r SHOULD_USE_GPS_INFO=''
+
 declare -r PICS_SRC_DIR='pics'
 declare -r BUILD_DIR='build'
 declare -r PICS_DIR_IN_BUILD="$PICS_SRC_DIR"
 declare -r OUT_PICS_DIR="$BUILD_DIR"/pics
 declare -r DESCR_DIR='descriptions'
+# First “%s”: Latitude. Second: Longitude. Both are in decimal degrees.
+declare -r GPS_URL_PATTERN='http://www.openstreetmap.org/?mlat=%s&mlon=%s&zoom=18'
 
 cd "$(readlink -f "$0" | xargs dirname)"
 
@@ -36,6 +42,141 @@ function get_pic_description {
 }
 
 # $1    A JPG picture.
+#
+# Set gps_url to:   A URL allowing to see where the picture was taken,
+#                   or nothing if no valid GPS data can be found.
+# Set location to:  A text describing the location,
+#                   or nothing if no valid data can be found.
+function get_gps_url {
+    gps_url=''
+    location=''
+    
+    : "${1:?No picture.}"
+    
+    local metadata_file
+    
+    metadata_file=${PICS_SRC_DIR}/$(
+        basename "$1" | sed '
+            s/\.[^.]*$//
+        '
+    ).metadata
+    
+    if [ ! -r "$metadata_file" ]
+    then
+        printf '%s: Warning: Could not find metadata file: %q\n' \
+                "$(basename "$0")" "$metadata_file" >&2
+        return 0
+    fi
+    
+    local lat_type
+    local lat_raw
+    local lon_type
+    local lon_raw
+    local gps_status
+    
+    local tag_id
+    local x
+    local value
+    
+    # Example:
+    #   Exif.Photo.WhiteBalance   Short   1   Auto
+    # We ignore two fields via the “x”.
+    while read tag_id x x value
+    do
+        case "$tag_id" in
+            Exif.GPSInfo.GPSLatitudeRef)
+                lat_type=$value
+                ;;
+            
+            Exif.GPSInfo.GPSLatitude)
+                lat_raw=$value
+                ;;
+            
+            Exif.GPSInfo.GPSLongitudeRef)
+                lon_type=$value
+                ;;
+            
+            Exif.GPSInfo.GPSLongitude)
+                lon_raw=$value
+                ;;
+            
+            Exif.GPSInfo.GPSStatus)
+                gps_status=$value
+                ;;
+            
+            Exif.Panasonic.Location|Exif.Panasonic.Country|Exif.Panasonic.State|Exif.Panasonic.City|Exif.Panasonic.Landmark)
+                if [ "$value" ] && [ "$value" != '---' ]
+                then
+                    if [ "$location" ]
+                    then
+                        location="$value, $location"
+                    else
+                        location=$value
+                    fi
+                fi
+                ;;
+            
+            *)
+                # Ignored.
+                ;;
+        esac
+    done < "$metadata_file"
+    
+    # Give up if no useful data.
+    if [ -z "$lat_type" ] || [ -z "$lat_raw" ] ||
+        [ -z "$lon_type" ] || [ -z "$lon_raw" ] ||
+        [ "$gps_status" != 'Measurement in progress' ]
+    then
+        return 0
+    fi
+    
+    local factor
+    
+    local d
+    local m
+    local s
+    
+    local lat_dd
+    local lon_dd
+    
+    if [ "$lat_type" = 'North' ]
+    then
+        factor=1
+    else
+        factor=-1
+    fi
+    
+    # “34deg 59' 55.530"” → “34 59 55.530”
+    read d m s < <(
+        tr -cd '0-9. ' <<< "$lat_raw"
+    )
+    lat_dd=$(
+        bc -l <<< "$factor * ($d + $m / 60 + $s / 3600)"
+    )
+    
+    if [ "$lon_type" = 'East' ]
+    then
+        factor=1
+    else
+        factor=-1
+    fi
+    
+    read d m s < <(
+        tr -cd '0-9. ' <<< "$lon_raw"
+    )
+    lon_dd=$(
+        bc -l <<< "$factor * ($d + $m / 60 + $s / 3600)"
+    )
+    
+    if [ "$lat_dd" ] && [ "$lon_dd" ]
+    then
+        gps_url=$(
+            printf "${GPS_URL_PATTERN}" "$lat_dd" "$lon_dd"
+        )
+    fi
+}
+
+# $1    A JPG picture.
 # stdout → A string containing some EXIF metadata, if available.
 function get_metadata_string {
     : ${1:?}
@@ -43,6 +184,14 @@ function get_metadata_string {
     local fnum=$( exif -mt 'F-Number' "$1" )
     local expt=$( exif -mt 'Exposure Time' "$1" )
     local isos=$( exif -mt 'ISO Speed Ratings' "$1" )
+    
+    local gps_url
+    local location
+    
+    if [ "$SHOULD_USE_GPS_INFO" ]
+    then
+        get_gps_url "$1"
+    fi
     
     local res
     
@@ -61,6 +210,17 @@ function get_metadata_string {
     then
         test "$res" && res+='  '
         res+="ISO $isos"
+    fi
+    if [ "$gps_url" ]
+    then
+        test "$res" && res+='  '
+        res+="<a class=\"gps-link\" target=\"_blank\" href=\"${gps_url}\">&#x1F310</a>"
+    fi+="ISO $isos"
+    fi
+    if [ "$location" ]
+    then
+        test "$res" && res+='<br>'
+        res+=$location
     fi
     
     printf '%s\n' "$res"
